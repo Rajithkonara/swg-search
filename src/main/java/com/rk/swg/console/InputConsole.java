0c598397470073f5d87559e810536ba5ab8541bf
Rajithkonara <rajithk@wso2.com>
2019-09-19 00:30:26 +0530

    adding console

---
+++ src/main/java/com/rk/swg/console/InputConsole.java
@@ -0,0 +1,89 @@
+package com.rk.swg.console;
+
+import com.google.gson.Gson;
+import com.rk.swg.dto.Organization;
+import com.rk.swg.dto.SearchResults;
+import com.rk.swg.dto.Ticket;
+import com.rk.swg.dto.User;
+import com.rk.swg.exception.IllegalInputExeception;
+import com.rk.swg.search.SearchFactory;
+import org.apache.log4j.Logger;
+
+import java.lang.reflect.Field;
+import java.util.*;
+
+public class InputConsole {
+
+    private static final Logger logger = Logger.getLogger(InputConsole.class);
+
+    public static void launchSearchTerminal() throws IllegalInputExeception {
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+                logger.info("starting the application ..... ");
+
+                System.out.println("Select \n 1 for Users \n 2 For Tickets \n 3 For Organizations \n");
+
+                String choice = scanner.next();
+                int choiceNo = Integer.parseInt(choice);
+
+                if (choiceNo < 1 || choiceNo > 3) {
+                    throw new IllegalInputExeception();
+                }
+
+                System.out.println("Enter the field to search :  ");
+                String fieldValue = scanner.next();
+
+                //validation
+                boolean isValid = false;
+
+                if (choiceNo == 1) {
+                    isValid = validateFields(fieldValue, User.class);
+                } else if (choiceNo == 2) {
+                    isValid = validateFields(fieldValue, Ticket.class);
+                } else if (choiceNo == 3) {
+                    isValid = validateFields(fieldValue, Organization.class);
+                }
+
+                if (isValid) {
+                    System.out.println("Enter Search Value : ");
+                    String searchValue = scanner.next();
+                    SearchResults print = SearchFactory.getInstance().search(choiceNo, fieldValue, searchValue);
+                    Gson gson = new Gson();
+                    String toJson = gson.toJson(print.getUserSearchResult());
+                    System.out.println(toJson);
+                    logger.info(toJson);
+
+                } else {
+                    System.out.println("Invalid input");
+                    logger.info("Invalid Input");
+                }
+
+            System.out.println("Enter N for new search or any other key to exit ");
+            boolean continueSearch = scanner.next().equalsIgnoreCase("n");
+            if (!continueSearch) break;
+        }
+    }
+
+
+    /**
+     * Validate the Input fields
+     * @param userInput
+     * @param c
+     * @return boolean
+     */
+    private static boolean validateFields(String userInput, Class c) {
+
+        Field[] list = c.getDeclaredFields();
+
+        Map<Integer, String> keyValues = new HashMap<>();
+
+        for (int i = 0; i < list.length; i++) {
+            keyValues.put(i, list[i].getName());
+        }
+
+        return keyValues.containsValue(userInput);
+    }
+
+}
