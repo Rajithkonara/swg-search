2238e8449288ffe66581e2792ffa22d300c5dca1
Rajithkonara <rajithk@wso2.com>
2019-10-03 12:01:31 +0530

    implementing ticket search

--- src/main/java/com/rk/swg/dto/SearchResults.java
+++ src/main/java/com/rk/swg/dto/SearchResults.java
@@ -1,5 +1,6 @@
 package com.rk.swg.dto;
 
+import com.rk.swg.search.TicketSearch;
 import lombok.Getter;
 
 import java.util.List;
@@ -8,9 +9,15 @@ import java.util.List;
 public class SearchResults {
 
     private List<com.rk.swg.dto.UserBuilder> userSearchResult;
+    private List<com.rk.swg.dto.OrganizationSearchResultBuilder> orgSearchResult;
+    private List<TicketSearchResultBuilder> ticketSearchResult;
 
-    public SearchResults(List<com.rk.swg.dto.UserBuilder> userSearchResult) {
+    public SearchResults(List<com.rk.swg.dto.UserBuilder> userSearchResult,
+                         List<com.rk.swg.dto.OrganizationSearchResultBuilder> orgSearchResult,
+                         List<TicketSearchResultBuilder> ticketSearchResult) {
         this.userSearchResult = userSearchResult;
+        this.orgSearchResult = orgSearchResult;
+        this.ticketSearchResult = ticketSearchResult;
     }
 
     public static class UserBuilder {
@@ -25,8 +32,40 @@ public class SearchResults {
         }
 
         public SearchResults build() {
-            return new SearchResults(userSearchResult);
+            return new SearchResults(userSearchResult, null, null);
         }
+    }
+
+
+    public static class OrganizationBuilder {
+
+        List<com.rk.swg.dto.OrganizationSearchResultBuilder> orgSearchResult;
+
+        public OrganizationBuilder(){}
+
+        public OrganizationBuilder setOrgSearchResult(List<com.rk.swg.dto.OrganizationSearchResultBuilder> orgSearchResult) {
+            this.orgSearchResult = orgSearchResult;
+            return this;
+        }
+
+        public SearchResults build() {
+            return new SearchResults(null, orgSearchResult, null);
+        }
+    }
+
+    public static class TicketBuilder {
+
+        List<TicketSearchResultBuilder> ticketSearchResult;
+
+        public TicketBuilder() {}
+
+        public TicketBuilder setTicketSearchResult(List<TicketSearchResultBuilder> ticketSearchResult) {
+            this.ticketSearchResult = ticketSearchResult;
+            return this;
+        }
+
+        public SearchResults build() { return new SearchResults(null, null, ticketSearchResult);  }
+
     }
 
 }
--- src/main/java/com/rk/swg/search/TicketSearch.java
+++ src/main/java/com/rk/swg/search/TicketSearch.java
@@ -1,21 +1,68 @@
 package com.rk.swg.search;
 
-import com.rk.swg.dto.SearchResults;
-import com.rk.swg.dto.TicketBuilder;
+import com.rk.swg.dto.*;
 import com.rk.swg.util.DataHolder;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.log4j.Logger;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class TicketSearch implements Search {
 
     private static final Logger logger = Logger.getLogger(TicketSearch.class);
 
+    private UserSearch userSearch;
+
     @Override
     public SearchResults search(String fieldName, String fieldValue) {
-        logger.info("Service not yet Implemented ");
+
+        try {
+
+            userSearch = new UserSearch();
+            List<UserBuilder> getAllUsers = userSearch.getAllUsers();
+
+            List<TicketSearchResultBuilder> getUsersAndOrg = getUsersAndOrganization(getAllUsers);
+
+            List<TicketSearchResultBuilder> searchResults = getUsersAndOrg.stream().
+                    map(p -> Pair.of(p.getTicket().get_id(), p)).map(
+                            p -> {
+                                Field field = null;
+                                try {
+                                    field = p.getRight().getTicket().getClass().getDeclaredField(fieldName);
+                                    field.setAccessible(true);
+                                } catch (NoSuchFieldException e) {
+                                    e.printStackTrace();
+                                }
+                                Object object = null;
+                                try {
+                                    object = field.get(p.getRight().getTicket());
+                                } catch (IllegalAccessException e) {
+                                    logger.error(e);
+                                }
+                                return Pair.of(object, p.getRight());
+                            }).filter(left -> Objects.nonNull(left.getLeft())).filter(
+                    q -> {
+                        if (q.getLeft() instanceof ArrayList) {
+                            ArrayList<String> left = (ArrayList<String>) q.getLeft();
+                            return left.contains(fieldValue);
+                        } else if (q.getLeft() instanceof Boolean) {
+                            return Boolean.parseBoolean(Boolean.toString((Boolean) q.getLeft()));
+                        } else {
+                            return q.getLeft().equals(fieldValue);
+                        }
+                        }).map(Pair::getRight).collect(Collectors.toList());
+
+                return new SearchResults.TicketBuilder().setTicketSearchResult(searchResults).build();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 
@@ -34,4 +81,50 @@ public class TicketSearch implements Search {
                              .build())
                 .collect(Collectors.toList());
     }
+
+
+    List<TicketSearchResultBuilder> getUsersAndOrganization(List<UserBuilder> users) throws IOException {
+
+        List<TicketSearchResultBuilder> ticketSearchResult = new ArrayList<>();
+
+
+        DataHolder.getInstance().getTickets().forEach(
+
+                ticket -> {
+
+                    List<UserRefBuilder> assigneddUsers = users.stream().filter(
+                            user ->
+                                    user.getUser().get_id().equals(ticket.getAssignee_id())).
+                            map(
+                              user -> new UserRefBuilder.Builder().setassigneeName(user.getUser().getName()).
+                                      build()).collect(Collectors.toList());
+
+                    List<UserRefBuilder> submitterUsers = users.stream().filter(
+                            user ->
+                                    ticket.getSubmitter_id().equals(user.getUser().get_id())).
+                            map(
+                                    user -> new UserRefBuilder.Builder().setSubmitterName(user.getUser().getName()).
+                                            build()).collect(Collectors.toList());
+
+                    try {
+                        List<String> organizations = DataHolder.getInstance().getOrganizations().stream().filter(
+
+                                organization ->
+                                        organization.get_id().equals(ticket.getOrganization_id())
+                        ).map(Organization::getName).collect(Collectors.toList());
+
+                        ticketSearchResult.add(new TicketSearchResultBuilder.Builder().setTicket(ticket)
+                                .setAssigneeName(assigneddUsers).setSubmitterName(submitterUsers).
+                                        setOrganizationName(String.valueOf(organizations)).build()
+
+                        );
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+
+            return ticketSearchResult;
+    }
+
 }
